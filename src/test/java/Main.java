c4e88ac7f0be50eb62392f494d79f24839fd4ae6
Kimberly Marin <KimberlyMarinVargas11@gmail.com>
2021-05-07 20:05:03 -0500

    implement test

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,4 @@
-import javax.swing.*;
-import java.util.Random;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -17,16 +16,59 @@ public class Main {
         String sPalabra = nombre;
 
         /*se crea contador para saber la cantidad de vocales*/
+        int contadorA = 0;
+        int contadorE = 0;
+        int contadorI = 0;
+        int contadorO = 0;
+        int contadorU = 0;
         int contador = 0;
+
+        ArrayList<String> vocales = new ArrayList<String>();
         /*se realiza el filtro de la palabra escrita y se almacena el contador */
         long totalCharacters = sPalabra.chars().filter(ch -> ch != ' ').count();
         /*se recorre la palabra por su longuitud para determinar la cantidad de letras*/
         for(int x=0;x<sPalabra.length();x++) {
-            if ((sPalabra.charAt(x)=='a') || (sPalabra.charAt(x)=='e') || (sPalabra.charAt(x)=='i') || (sPalabra.charAt(x)=='o') || (sPalabra.charAt(x)=='u')){
-                contador++;
+            switch (sPalabra.charAt(x)){
+                case 'a':
+                    if(contadorA<1){
+                        vocales.add("a");
+                    }
+                    contadorA++;
+                    contador++;
+                    break;
+                case 'e':
+                    if(contadorE<1){
+                        vocales.add("e");
+                    }
+                    contadorE++;
+                    contador++;
+                    break;
+                case 'i':
+                    if(contadorI<1){
+                        vocales.add("i");
+                    }
+                    contadorI++;
+                    contador++;
+                    break;
+                case 'o':
+                    if(contadorO<1){
+                        vocales.add("o");
+                    }
+                    contadorO++;
+                    contador++;
+                    break;
+                case 'u':
+                    if(contadorU<1){
+                        vocales.add("u");
+                    }
+                    contadorU++;
+                    contador++;
+                    break;
+
             }
         }
         /*Se imprime la cantidad de vocales y la longuitud de la palabra*/
+        System.out.println("las vocales son: " + vocales );
         System.out.println("La palabra " + sPalabra + " contiene " + contador + " vocales");
         System.out.println("la longuitud de la palabra es: " + totalCharacters );
 
